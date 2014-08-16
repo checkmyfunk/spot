3bf43c9016ba4e5d828b708a33486b7f2780fd0a
check <vitali.potchekin@gmail.com>
2014-08-16 15:43:32 -0400

    Project Creation

---
+++ Spot/Spot/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Spot
+//
+//  Created by Vitali Potchekin on 8/16/14.
+//  Copyright (c) 2014 Vitali Potchekin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
